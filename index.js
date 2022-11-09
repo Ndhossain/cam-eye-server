7dfd2f1f56422ac28f3e13b75ca9c3abc2a2cedf
Ndhossain <hhhssnahiid@gmail.com>
2022-11-09 18:39:46 +0600

    Service api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
@@ -18,13 +18,32 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        const db = client.db('cam-eye');
+        // jwt for logged in user
         app.post('/jwt', (req, res) => {
             const data = req.body;
             if(!data?.uid){
-                return res.status(403).send('forbidden')
+                return res.status(403).send('forbidden');
             }
             const token = jwt.sign(data, process.env.SECRET_KEY, { expiresIn: '1h' })
             res.send({jwt: token});
+        })
+        // services
+        const servcesCollection = db.collection('service');
+        app.get('/services', async (req, res) => {
+            const page = Number(req.query.page);
+            const size = Number(req.query.size);
+            console.log(page, size);
+            const query = {};
+            const count = await servcesCollection.countDocuments()
+            const cursor = await servcesCollection.find(query).skip(size * page).limit(size).toArray();
+            res.send({count, services: cursor});
+        })
+        app.get('/service-details/:id', async (req, res) => {
+            console.log(req.params)
+            const query = { _id: ObjectId(req.params.id) };
+            const cursor = await servcesCollection.findOne(query);
+            res.send(cursor || {})
         })
     } catch (err) {
         console.log(err);
